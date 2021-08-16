c879a1b74c07bf5a4ef1f301723e808fda1bc2f2
OURO-BANG'NA <ourobadiou@gmail.com>
2021-08-16 13:31:41 +0000

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -3,50 +3,53 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MyApp());
 }
-
 class MyApp extends StatelessWidget {
+  //const MyApp({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        home: Scaffold(
-            backgroundColor: Colors.teal,
-            body: SafeArea(
-                child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                  Container(
-                      width: 100.0, height: double.infinity, color: Colors.red),
-                  /*SizedBox(
-                    width: 50.0,
-                  ),*/
-                  Container(
-                    padding:EdgeInsets.symmetric(vertical: 170.0) ,
-                    child: Column(children: [
-                      Container(
-                          //margin:EdgeInsets.only(top: 200.0),
-                          height: 50.0,
-                          width:50.0,
-                          color:Colors.yellow
-                      ),
-                      Container(
-                          //margin:EdgeInsets.only(bottom: 200.0),
-                          height: 50.0,
-                          width:50.0,
-                          color:Colors.greenAccent
-                      ),
-                    ]),
-                    height: double.infinity,
-                    color: Colors.teal,
-                  ),
-                  /*SizedBox(
-                    width: 50.0,
-                  ),*/
-                  Container(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            //crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Container(
+                width: 100.0,
+                height: double.infinity,
+                color: Colors.white,
+                child: Text('Container 1'),
+              ),
+              //SizedBox(width: 10.0),
+              Container(
+                child:Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children:[
+                    Container(
+                      color:Colors.yellow,
+                      height: 100.0,
                       width: 100.0,
-                      height: double.infinity,
-                      color: Colors.blue)
-
-                ]))));
+                    ),
+                    Container(
+                      color:Colors.lightGreenAccent,
+                      height: 100.0,
+                      width: 100.0,
+                    ),
+                  ]
+                ),
+              ),
+              Container(
+                width: 100.0,
+                height: double.infinity,
+                color: Colors.red,
+                child: Text('Container 2'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
